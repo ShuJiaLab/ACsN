056ee1627b98c404606df4b9bd6b6bc7afd29c24
The Jia Laboratory @ Georgia Tech <46298520+ShuJiaLab@users.noreply.github.com>
2020-01-10 12:10:14 -0500

    ImageJ script update
    Now offset and gain map can be loaded from .mat files. They can be in different files or the same as long as they have been saved in variables called offset and gain, respectively.

--- ACsN_ImageJ/ACsN_.m
+++ ACsN_ImageJ/ACsN_.m
@@ -7,11 +7,11 @@
 
 % @ String(choices={"auto", "yes", "no"}, style="radioButtonHorizontal") Video
 
-% @ String(label="Offset Choice", choices={"File", "Value"}, style="radioButtonHorizontal") OffsetChoice
+% @ String(label="Offset Choice", choices={"File (.mat)", "File (.tiff)", "Value"}, style="radioButtonHorizontal") OffsetChoice
 % @ File(label="Select a file:" , description="Select a file with the offset map", style="extensions:tiff/tif") OffsetFile
 % @ Float(label="Offset", description="Average offset value") OffsetValue
 
-% @ String(label="Gain Choice", choices={"File", "Value"}, style="radioButtonHorizontal") GainChoice
+% @ String(label="Gain Choice", choices={"File (.mat)", "File (.tiff)", "Value"}, style="radioButtonHorizontal") GainChoice
 % @ File(label="Select a file:" , description="Select a file with the gain map", style="extensions:tiff/tif") GainFile
 % @ Float(label="Gain", description="Average offset value") GainValue
 
@@ -20,32 +20,44 @@
 % offset
 %disp(OffsetChoice)
 if OffsetChoice(1) == 'F'
-clear offset;
-offset = double(imread(char(OffsetFile)));
+    if OffsetChoice(8) == 'm'
+        clear offset;
+        f = load(char(OffsetFile));
+        offset = f.offset;
+    else
+        clear offset;
+        offset = double(imread(char(OffsetFile)));
+    end
 else
-clear offset;
-offset = double(OffsetValue);
-%disp(offset)
+    clear offset;
+    offset = double(OffsetValue);
+    %disp(offset)
 end
 
 % gain
 %disp(GainChoice)
 if GainChoice(1) == 'F'
-clear gain;
-gain = double(imread(char(GainFile)));
+    if GainChoice(8) == 'm'
+        clear gain;
+        g = load(char(GainFile));
+        gain = g.gain;
+    else
+        clear gain;
+        gain = double(imread(char(GainFile)));
+    end
 else
-clear gain;
-gain = double(GainValue);
-%disp(gain)
+    clear gain;
+    gain = double(GainValue);
+    %disp(gain)
 end
 
 % parallel
 %disp(ParChoice)
 if ParChoice(1) == 'n'
-Mode = 'Normal';
+    Mode = 'Normal';
 else
-Mode = 'Fast';
-%disp(parallel)
+    Mode = 'Parallel';
+    %disp(parallel)
 end
 
 
